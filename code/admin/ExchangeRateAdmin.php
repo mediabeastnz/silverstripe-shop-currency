876c4d916c74d0b9ccb787e477169d3a95cebe4d
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:38:58 +1300

    Converted to PSR-2

--- code/admin/ExchangeRateAdmin.php
+++ code/admin/ExchangeRateAdmin.php
@@ -1,22 +1,24 @@
 <?php
-class ExchangeRateAdmin extends ModelAdmin {
+class ExchangeRateAdmin extends ModelAdmin
+{
 
-  private static $managed_models = array('ExchangeRate');
-  private static $url_segment = 'exchangeRate';
-  private static $menu_title = 'Exchange Rates';
+    private static $managed_models = array('ExchangeRate');
+    private static $url_segment = 'exchangeRate';
+    private static $menu_title = 'Exchange Rates';
   //private static $menu_priority = 100;
   //private static $menu_icon = 'mysite/images/cashmoneys.png';
   private static $model_importers = array();
 
-  public function getEditForm($id = null, $fields = null) {
-    $form = parent::getEditForm($id, $fields);
+    public function getEditForm($id = null, $fields = null)
+    {
+        $form = parent::getEditForm($id, $fields);
 
-    $gridField = $form->Fields()->fieldByName($this->sanitiseClassName($this->modelClass));
-    $Config = $gridField->getConfig();
+        $gridField = $form->Fields()->fieldByName($this->sanitiseClassName($this->modelClass));
+        $Config = $gridField->getConfig();
 
-    $Config->removeComponentsByType('GridFieldDeleteAction');
-    $gridField->setConfig($Config);
+        $Config->removeComponentsByType('GridFieldDeleteAction');
+        $gridField->setConfig($Config);
 
-    return $form;
-  }
+        return $form;
+    }
 }
